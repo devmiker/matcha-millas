edf85011f0c63ec2a84ff9a837a378223e38400d
Mike Rodriguez <devmiker@gmail.com>
2025-06-30 16:22:25 +0200

    Added User.cs model and AuthDbContext and connection string to appsettings and updated Program.cs file.

--- services/auth-service/AuthService/Program.cs
+++ services/auth-service/AuthService/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectString("DefaultConnection")));
 
 var app = builder.Build();
 
